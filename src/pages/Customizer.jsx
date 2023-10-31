6a67e66366b8fed282b9047345e020ed86a4f32b
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-10-31 18:31:10 +0530

    made the required components n imported them for customization

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import {AnimatePresence, motion } from 'framer-motion'
+import {useSnapshot } from 'valtio'
+
+import config from '../config/config/config'
+import state from '../store'
+import { download  } from '../assets'
+import { downloadCanvasToImage, reader } from '../config/config/helpers'
+import { EditorTab, FilterTab ,DecalTypes } from '../config/config/constants'
+import { fadeAnimation, slideAnimation } from '../config/config/motion'
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components'
 
 const Customizer = () => {
   return (
