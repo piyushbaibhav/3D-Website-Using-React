6c694f9345b41471947d07ed4058491bda73a549
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-11-01 07:26:32 +0530

    customizer animation return & constant file path issue sloved

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -4,15 +4,24 @@ import {useSnapshot } from 'valtio'
 
 import config from '../config/config/config'
 import state from '../store'
-import { download  } from '../assets'
+import { download  } from '../assets/assets'
 import { downloadCanvasToImage, reader } from '../config/config/helpers'
-import { EditorTab, FilterTab ,DecalTypes } from '../config/config/constants'
+import { EditorTabs, FilterTabs, DecalTypes } from "../config/config/constants";
 import { fadeAnimation, slideAnimation } from '../config/config/motion'
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components'
 
 const Customizer = () => {
+   
+  const snap = useSnapshot (state);
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+      {!snap.intro &&(
+        <>
+          Customizer
+
+        </>
+      )}
+    </AnimatePresence>
   )
 }
 
