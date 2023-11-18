dbf5b76e92e3451bfca624b724318d84a5c0af99
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-11-19 04:16:40 +0530

    solved prev error but got new one

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,16 +1,29 @@
-import {Canvas} from '@react-three/fiber'
-import {Environment, Center} from '@react-three/drei';
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-import Shirt from './Shirt';
-import BackDrop from './Backdrop';
-import CameraRig from './CameraRig';
-
-
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
 
 const CanvasModel = () => {
   return (
-    <div>Canvas</div>
-  )
-}
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
 
-export default CanvasModel
+      {/* <CameraRig> */}
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CameraRig> */}
+    </Canvas>
+  );
+};
+
+export default CanvasModel;
